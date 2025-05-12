c3899142fee62b3579db8ef02bc25019bb0b0b00
Altaxia <leiapercherancier@gmail.com>
2025-05-12 15:12:53 +0200

    assets

--- Fill Your Pockets/Assets/Scripts/Chest.cs
+++ Fill Your Pockets/Assets/Scripts/Chest.cs
@@ -1,30 +1,60 @@
+using System.Collections;
 using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
     public int goldPerInterval = 5;
     public float intervalSeconds = 3f;
+    public float interactionDistance = 1.5f; // Distance pour interagir avec le coffre
 
     private float timer = 0f;
     private int storeGold = 0;
+    private Animator animator;
+
+    private GameObject player;
+    private bool isOpened = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void Update()
     {
-        timer += Time.deltaTime;
 
+        timer += Time.deltaTime;
         if (timer >= intervalSeconds)
         {
             storeGold += goldPerInterval;
             timer = 0f;
+        }
+
+        if (!isOpened && Input.GetKeyDown(KeyCode.F))
+        {
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+
+            if (distance <= interactionDistance)
+            {
+                GetGold();
+                isOpened = true;
+            }
         }
     }
 
     public int GetGold()
     {
         int goldToGive = storeGold;
-        Destroy(gameObject);
+        animator.SetTrigger("Open");
+        StartCoroutine(DestroyAfterAnimation());
+        animator.SetBool("Empty", true);
+        return goldToGive;
+    }
 
-       return goldToGive;
+    private IEnumerator DestroyAfterAnimation()
+    {
+        yield return new WaitForSeconds(1.0f); 
+        Destroy(gameObject);
     }
 
     public int getStoreGold()
