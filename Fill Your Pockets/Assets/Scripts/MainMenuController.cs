2e31e626d8474af3e0484e4534ee961f63a4c98a
Altaxia <leiapercherancier@gmail.com>
2025-05-14 17:44:07 +0200

    menu

---
+++ Fill Your Pockets/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject tutorialPopup; 
+    public GameObject tutorial; 
+
+    private void Start()
+    {   
+        tutorialPopup.SetActive(false); 
+    }
+
+    public void OnGameButtonClicked()
+    {
+        Debug.Log("oe ça marche");
+        tutorialPopup.SetActive(true); 
+    }
+
+    public void OnSaveButtonClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnSkipTutorial()
+    {
+        SceneManager.LoadScene("GameScene");
+        Debug.Log("oe ça marche");
+    }
+
+
+    public void StartGame(){
+        Debug.Log("ça marche");
+        SceneManager.LoadScene("GameScene");
+    }
+    public void OnStartTutorial()
+    {
+        tutorial.SetActive(true); 
+
+        StartCoroutine(EndTutorialAndLoadGameScene());
+    }
+
+    public IEnumerator EndTutorialAndLoadGameScene()
+    {
+        yield return new WaitForSeconds(5f); 
+        SceneManager.LoadScene("GameScene");
+    }
+}
